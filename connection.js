8ba683c7c188ed5092a8b7f81cb67019d32c758d
Danar <danarcahyadi21@gmail.com>
2023-04-03 08:25:40 +0800

    Config mysql connection

--- connection.js
+++ connection.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2')
 require("dotenv").config()
 const conn = mysql.createConnection({
-    host : process.env.HOST,
-    user : process.env.USER,
-    password : process.env.PASSWORD,
-    database : process.env.DATABASE,
-    port : process.env.DB_PORT
+    host : process.env.MYSQLHOST,
+    user : process.env.MYSQLUSER,
+    password : process.env.MYSQLPASSWORD,
+    database : process.env.MYSQLDATABASE,
+    port : process.env.MYSQLPORT
 
 })
 
